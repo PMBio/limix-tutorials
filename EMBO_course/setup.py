b38c04a3685dd4fc4aed096638fb5bab3bf7d791
Oliver Stegle <oliver.stegle@ebi.ac.uk>
2014-07-25 13:59:40 +0100

    updated'

---
+++ EMBO_course/setup.py
@@ -0,0 +1,24 @@
+import scipy as sp
+import pylab as pl
+from matplotlib import cm
+import scipy.stats as st
+import h5py
+import pdb
+import pandas as pd
+sp.random.seed(0)
+# import LIMIX
+import sys
+import limix.modules.varianceDecomposition as var
+import limix.modules.qtl as qtl
+import limix.io.data as data
+import limix.io.genotype_reader as gr
+import limix.io.phenotype_reader as phr
+import limix.io.data_util as data_util
+import limix.utils.preprocess as preprocess
+# plotting and visualization utilties
+from limix.utils.plot import *
+import os
+import cPickle
+import sys
+import numpy as np
+import pandas as pd
