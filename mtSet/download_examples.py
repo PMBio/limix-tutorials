edaf93f6c2d3f2a5d1a743109f3a7f5d2d2566a2
Danilo Horta <danilo.horta@gmail.com>
2016-06-22 18:42:17 +0100

    download examples for python 3

--- mtSet/download_examples.py
+++ mtSet/download_examples.py
@@ -5,7 +5,12 @@ from os.path import exists
 from os.path import dirname
 from os.path import abspath
 
-import urllib
+try:
+    from urllib.request import urlopen
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+    from urllib import urlopen
 
 _root = dirname(abspath(__file__))
 
@@ -17,10 +22,10 @@ def download_file(indir, fname, url):
             os.makedirs(indir)
 
         _url = join(url, fname)
-        testfile = urllib.URLopener()
-
         print("Downloading file %s to %s" % (_url, full_name))
-        testfile.retrieve(_url, full_name)
+        u = urlopen(_url)
+        with open(full_name, 'w') as f:
+            f.write(u.read())
 
 def get_1000G_mtSet():
     indir = join(_root, 'data', '1000g')
